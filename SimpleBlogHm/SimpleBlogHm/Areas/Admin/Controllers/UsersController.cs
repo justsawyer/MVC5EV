c1cb601de9d6988d165cf04b80e0f1425074f7ab
OKUNAN\TUNAHAN <tunahanokunan>
2017-02-25 12:47:04 +0300

    Second Commit

---
+++ SimpleBlogHm/SimpleBlogHm/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleBlogHm.Areas.Admin.Controllers
+{
+    public class UsersController : Controller
+    {
+        //
+        // GET: /Admin/Users/
+        public ActionResult Index()
+        {
+            return Content("Users Index Screen");
+        }
+        public ActionResult List()
+        {
+            return Content("Users List Screen");
+        }
+        public ActionResult Edit(int id=0)
+        {
+            return Content("Users Edit="+id);
+        }
+	}
+}
